cad47f59c342b27f4ec7a1f2430fce0eb37a7ddc
QALLOUJ <qalloujchafae1409@gmail.com>
2025-01-28 17:15:25 +0100

    update

---
+++ memoire.php
@@ -0,0 +1,25 @@
+<?php
+
+// Démarrer la session
+session_start();
+
+
+require 'vendor/autoload.php'; // Autoloader pour Twig
+
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
+
+// Initialiser Twig
+$loader = new FilesystemLoader('templates'); // Assurez-vous que 'templates' contient 'login.html.twig'
+$twig = new Environment($loader);
+
+
+// Définir la page active
+$pageActive = 'jeux'; 
+
+// Afficher la page de connexion (formulaire)
+echo $twig->render('memoire.html.twig', [
+    'pageActive' => $pageActive,
+]);
+?>
+
